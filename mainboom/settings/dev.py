0ceaf185b7dbd65f288d8fe5f248f6b670e59e8b
Claudron <claudiowudang@gmail.com>
2022-10-21 12:41:17 +0200

    addes untracked files

--- mainboom/settings/dev.py
+++ mainboom/settings/dev.py
@@ -7,9 +7,21 @@ DEBUG = True
 SECRET_KEY = 'django-insecure-led64k5qbjz!92b$8f$sh+(=7f%k$2*9q1er@psvxkp94+qjq3'
 
 # SECURITY WARNING: define the correct hosts in production!
-ALLOWED_HOSTS = ['*'] 
+ALLOWED_HOSTS = ['*']
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+INSTALLED_APPS += [
+    'debug_toolbar',
+]
+
+MIDDLEWARE += [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+]
+
+INTERNAL_IPS = [
+    '127.0.0.1'
+]
 
 
 try:
