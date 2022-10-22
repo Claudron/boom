9790e30ebc4e32fb7a7d57600edbff3796c0c01f
Claudron <claudiowudang@gmail.com>
2022-10-22 16:45:02 +0200

    included links via link class into Card class to simplify code. added the stream field functionallity and  better html to cards_block.html

--- streams/blocks.py
+++ streams/blocks.py
@@ -17,19 +17,38 @@ class TitleBlock(blocks.StructBlock):
         help_text = "Centered text to display on the page"
 
 
+class Link(blocks.StructBlock):
+
+    link_text = blocks.CharBlock(max_length=50,
+                                 default='more details',
+                                 )
+    internal_page = blocks.PageChooserBlock(required=False,
+                                            )
+    external_link = blocks.URLBlock(required=False,
+                                    )
+
+
+class Card(blocks.StructBlock):
+
+    title = blocks.CharBlock(max_length=100,
+                             help_text="Bold title text for this card. Max length of 100 characters.",
+                             )
+    text = blocks.TextBlock(max_length=225,
+                            required=False,
+                            help_text="Optional text for this card. Max length is 255 characters",
+                            )
+    image = ImageChooserBlock(required=False,
+                              help_text="image will be automatically cropped by 570px by 370px",
+                              )
+    link = Link(help_text="Enter a link or select a page",
+                )
+
+
+
 class CardsBlock(blocks.StructBlock):
 
     cards = blocks.ListBlock(
-        blocks.StructBlock(
-            [
-                ("title", blocks.CharBlock(max_length=100,
-                                           help_text="Bold title text for this card. Max length of 100 characters.")),
-                ("text", blocks.TextBlock(max_length=225, required=False,
-                                          help_text="Optional text for this card. Max length is 255 characters")),
-                ("image", ImageChooserBlock(required=False,
-                                            help_text="image will be automatically cropped by 570px by 370px"))
-            ]
-        )
+        Card()
     )
 
     class Meta:
