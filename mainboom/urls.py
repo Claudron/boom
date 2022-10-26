91737033ab481f3243d8f592ceed77e4ca07513d
Claudron <claudiowudang@gmail.com>
2022-10-26 21:02:38 +0200

    fix debug tool bar in urls.py

--- mainboom/urls.py
+++ mainboom/urls.py
@@ -28,13 +28,14 @@ if settings.DEBUG:
     # Serve static and media files from development server
     urlpatterns += staticfiles_urlpatterns()
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += [path('__debug__/', include(debug_toolbar.urls))]
+
 
 urlpatterns = urlpatterns + [
     # For anything not caught by a more specific rule above, hand over to
     # Wagtail's page serving mechanism. This should be the last pattern in
     # the list:
-    path('__debug__/', include(debug_toolbar.urls)),
-    path("", include(wagtail_urls)),
+    path("", include(wagtail_urls))
 
 
     # Alternatively, if you want Wagtail pages to be served from a subpath
