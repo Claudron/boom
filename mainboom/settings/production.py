968902ad1abbd55647fddaa7310261101aa0645b
Claudron <claudiowudang@gmail.com>
2022-10-27 15:18:56 +0200

    change production.py alowwed hosts to  empty and change its place in the code

--- mainboom/settings/production.py
+++ mainboom/settings/production.py
@@ -5,12 +5,13 @@ import dj_database_url
 DEBUG = 'RENDER' not in os.environ
 
 SECRET_KEY = os.environ["SECRET_KEY"]
-ALLOWED_HOSTS = ['localhost', 'boom.onrender.com']
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 
 if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+
+ALLOWED_HOSTS = []
 
 
 DATABASES = {
