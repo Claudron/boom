b64a17e00b5c4392c874455138c9779d7bd931de
Claudron <claudiowudang@gmail.com>
2022-10-31 23:10:14 +0100

    copy prod, base, dev from domma

--- mainboom/settings/dev.py
+++ mainboom/settings/dev.py
@@ -1,12 +1,5 @@
 from .base import *
 
-#raise RuntimeError("I am the DEV error message!")
-
-
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-led64k5qbjz!92b$8f$sh+(=7f%k$2*9q1er@psvxkp94+qjq3'
 
@@ -27,6 +20,9 @@ INTERNAL_IPS = [
     '127.0.0.1'
 ]
 
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -34,7 +30,3 @@ DATABASES = {
     }
 }
 
-try:
-    from .local import *
-except ImportError:
-    pass
--- mainboom/settings/production.py
+++ mainboom/settings/production.py
@@ -2,14 +2,11 @@ import os
 from .base import *
 import dj_database_url
 
-
-DEBUG = 'RENDER' not in os.environ
-
 ALLOWED_HOSTS = ["localhost"]
 
-RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+RENDER_EXTERNAL_HOSTNAME = os.environ["RENDER_EXTERNAL_HOSTNAME"]
 
-SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
@@ -22,23 +19,39 @@ DATABASES = {
     )
 }
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': os.environ.get("LOG_LEVEL", "WARNING").upper(),
+    },
+}
 
-import sentry_sdk
-from sentry_sdk.integrations.django import DjangoIntegration
+#   Check if SENTRY_DSN is set. If yes, import sentry modules and set
+#   everything up. If not, just run without sentry.
+sentry_dsn = os.environ.get("SENTRY_DSN")
+if sentry_dsn:
+    import sentry_sdk
+    from sentry_sdk.integrations.django import DjangoIntegration
 
-sentry_sdk.init(
-    dsn="https://7cc3da3eac1b46a68be7aece966250bd@o4504055604051968.ingest.sentry.io/4504055608311808",
-    integrations=[
-        DjangoIntegration(),
-    ],
+    sentry_sdk.init(
+        dsn=sentry_dsn,
+        integrations=[
+            DjangoIntegration(),
+        ],
 
-    # Set traces_sample_rate to 1.0 to capture 100%
-    # of transactions for performance monitoring.
-    # We recommend adjusting this value in production.
-    traces_sample_rate=1.0,
+        # Set traces_sample_rate to 1.0 to capture 100%
+        # of transactions for performance monitoring.
+        # We recommend adjusting this value in production.
+        traces_sample_rate=1.0,
 
-    # If you wish to associate users to errors (assuming you are using
-    # django.contrib.auth) you may enable sending PII data.
-    send_default_pii=True
-)
-
+        # If you wish to associate users to errors (assuming you are using
+        # django.contrib.auth) you may enable sending PII data.
+        send_default_pii=True
+    )
