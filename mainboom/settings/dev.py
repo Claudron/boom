81cc79555a93a59729bacdc1de9ccec4d9e0dd2e
Achim Domma <achim@domma.de>
2022-10-31 17:16:10 +0100

    minor cleanup. usage of DATABASE_URL

--- mainboom/settings/dev.py
+++ mainboom/settings/dev.py
@@ -1,12 +1,5 @@
 from .base import *
 
-#raise RuntimeError("I am the DEV error message!")
-
-
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-led64k5qbjz!92b$8f$sh+(=7f%k$2*9q1er@psvxkp94+qjq3'
 
@@ -26,9 +19,3 @@ MIDDLEWARE += [
 INTERNAL_IPS = [
     '127.0.0.1'
 ]
-
-
-try:
-    from .local import *
-except ImportError:
-    pass
--- mainboom/settings/production.py
+++ mainboom/settings/production.py
@@ -2,22 +2,9 @@ import os
 from .base import *
 import dj_database_url
 
-#raise RuntimeError("I am the PRODUCTION error message!")
+ALLOWED_HOSTS = ["localhost"]
 
-DEBUG = 'RENDER' not in os.environ
-SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
-cwd = os.getcwd()
-CACHES = {
-    "default": {
-        "BACKEND": "django.core.cache.backends.filebased.FileBasedCache",
-        "LOCATION": f"{cwd}/.cache"
-    }
-}
-
-
-ALLOWED_HOSTS = []
-
-RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+RENDER_EXTERNAL_HOSTNAME = os.environ["RENDER_EXTERNAL_HOSTNAME"]
 
 if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
@@ -25,52 +12,31 @@ if RENDER_EXTERNAL_HOSTNAME:
 
 DATABASES = {
     'default': dj_database_url.config(
-        default='postgresql://postgres:postgres@localhost/postgres',
+        default=os.environ["DATABASE_URL"],
         conn_max_age=600
     )
 }
 
 
-MIDDLEWARE += [
-    'whitenoise.middleware.WhiteNoiseMiddleware',
-]
+#   Check if SENTRY_DSN is set. If yes, import sentry modules and set
+#   everything up. If not, just run without sentry.
+sentry_dsn = os.environ.get("SENTRY_DSN")
+if sentry_dsn:
+    import sentry_sdk
+    from sentry_sdk.integrations.django import DjangoIntegration
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.0/howto/static-files/
+    sentry_sdk.init(
+        dsn=sentry_dsn,
+        integrations=[
+            DjangoIntegration(),
+        ],
 
-# This setting tells Django at which URL static files are going to be served to the user.
-# Here, they will be accessible at your-domain.onrender.com/static/...
-STATIC_URL = '/static/'
-# Following settings only make sense on production and may break development environments.
-if not DEBUG:    # Tell Django to copy statics to the `staticfiles` directory
-    # in your application directory on Render.
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    # Turn on WhiteNoise storage backend that takes care of compressing static files
-    # and creating unique names for each version, so they can safely be cached forever.
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+        # Set traces_sample_rate to 1.0 to capture 100%
+        # of transactions for performance monitoring.
+        # We recommend adjusting this value in production.
+        traces_sample_rate=1.0,
 
-
-import sentry_sdk
-from sentry_sdk.integrations.django import DjangoIntegration
-
-sentry_sdk.init(
-    dsn="https://7cc3da3eac1b46a68be7aece966250bd@o4504055604051968.ingest.sentry.io/4504055608311808",
-    integrations=[
-        DjangoIntegration(),
-    ],
-
-    # Set traces_sample_rate to 1.0 to capture 100%
-    # of transactions for performance monitoring.
-    # We recommend adjusting this value in production.
-    traces_sample_rate=1.0,
-
-    # If you wish to associate users to errors (assuming you are using
-    # django.contrib.auth) you may enable sending PII data.
-    send_default_pii=True
-)
-
-try:
-    from .local import *
-except ImportError:
-    pass
-
+        # If you wish to associate users to errors (assuming you are using
+        # django.contrib.auth) you may enable sending PII data.
+        send_default_pii=True
+    )
