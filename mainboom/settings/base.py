b3c0a32e864e20529bfaa3a6425e8e6dc78d2892
Claudron <claudiowudang@gmail.com>
2022-10-31 16:33:07 +0100

    change database in base.py to dev

--- mainboom/settings/base.py
+++ mainboom/settings/base.py
@@ -96,12 +96,7 @@ WSGI_APPLICATION = 'mainboom.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+
 
 
 AUTH_PASSWORD_VALIDATORS = [
--- mainboom/settings/dev.py
+++ mainboom/settings/dev.py
@@ -27,6 +27,13 @@ INTERNAL_IPS = [
     '127.0.0.1'
 ]
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
@@ -47,6 +54,8 @@ LOGGING = {
         },
     },
 }
+
+
 
 
 
